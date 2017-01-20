0617abffdf8b40b190fb37ca7d471a07903ceb9c
nappdaddy2000 <mnapper3@ivytech.edu>
2017-01-20 02:51:32 +0000

    add creation process for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
+    before_action :set_posts, only: [:show]
+    
     def index
+    end
+    
+     def new
+         @post = Post.new
+     end
+     
+     def create
+        @post = Post.new(post_params)
+        if @post.save
+            # if saved redirect to our show page, post_path(@post)
+            redirect_to post_path(@post), notice: 'Post was successfully created.'
+        else
+            render :new
+        end
+     end
+     
+     def show
+         set_posts
+     end
+     
+private
+    
+    def post_params
+        params.require(:post).permit(:date, :rationale)
+    end
+    
+    def set_posts
+        @post = Post.find(params[:id])
     end
 end
