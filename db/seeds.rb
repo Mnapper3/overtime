2974bee2d8f75f97ea7ab3f6cfd31e61ea217058
nappdaddy2000 <mnapper3@ivytech.edu>
2017-01-29 11:42:06 +0000

    add enums and intergrated helper file for labels

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,27 @@
                     password_confirmation: "asdfasdf", 
                     first_name: "Jon", 
                     last_name: "Snow", 
-                    phone: "5747424510 ")
+                    phone: "5747424510")
 
 puts "1 user created"
 
 AdminUser.create(email: "admintest@test.com", 
-                password: "asdfasdf",
-                password_confirmation: "asdfasdf", 
-                first_name: "Admin", 
-                last_name: "Snow", 
-                phone: "5747424510 ")
+                        password: "asdfasdf",
+                        password_confirmation: "asdfasdf", 
+                        first_name: "Admin", 
+                        last_name: "Snow", 
+                        phone: "5747424510")
 
 puts "1 Admin User created"
 
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
 
 100.times do |audit_log|
-    AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
-puts "100 auditLogs have been created"
+puts "100 audit logs have been created"
