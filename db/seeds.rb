225804d87847d8f6bad6dc56ebcef65f7622b2df
nappdaddy2000 <mnapper3@ivytech.edu>
2017-01-30 10:23:00 +0000

    custom action buttons and policy for aprrove and review of post

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,8 @@ AdminUser.create(email: "admintest@test.com",
 puts "1 Admin User created"
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+<p>Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
